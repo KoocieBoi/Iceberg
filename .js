ab8fbd4ff1ff9bf85b117e6b4c3f39630dfe6eac
Koocie <koocieyu@gmail.com>
2018-10-26 21:28:18 +0300

    Added regions to main.js and .js

--- .js
+++ .js
@@ -1,3 +1,4 @@
+// #region Dependencies
 let config = require("./.data/config");
 let utils = require("./file/utils/main");
 let { on, login } = utils;
@@ -6,6 +7,7 @@ let Discord = require("discord.js");
 let events = {
    ready: require("./file/events/ready")
 };
+// #endregion
 
 login(config.credentials.token);
 on("ready", () => { events.ready(); });
--- file/utils/main.js
+++ file/utils/main.js
@@ -1,8 +1,14 @@
 let ds = require("discord.js");
 let cl = new ds.Client();
 
+
+// #region d.js main
 exports.login = (token) => cl.login(token);
 exports.on = (event, func) => cl.on(event, func);
+// #endregion
+
+// #region JS
 exports.isArr = (array) => { return Array.isArray(array); };
 exports.isObj = (object) => { return !Array.isArray(object) && typeof object === "object"; };
 exports.isStr = (string) => { return typeof string === "string"; };
+// #endregion
